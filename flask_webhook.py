b5b22a1d669e0483f1e0b15c8455aaf5d400720a
oriverav <63879721+oriverav@users.noreply.github.com>
2021-01-12 15:56:23 -0600

    Create flask_webhook.py

---
+++ flask_webhook.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+# '/webhook' URL where the webhook will be running in the server
+# POST means that the webhook will only hear to POST requests
+@app.route('/webhook', methods=['POST'])
+def respond():
+	print(request.json)
+	print(request.json['ciscoDnaEventLink'])
+	return Response(status=400) 
+
+# The app will run in all the IPs of your computer in port 5000 by default.
+# App will be running with HTTPS
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', ssl_context='adhoc')
